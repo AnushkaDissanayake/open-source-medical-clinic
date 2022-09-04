33e756c11b5ca3e6cbcc93394a2e9820d9ab6ea2
AnushkaDissanayake <anushkamadusanka5@gmail.com>
2022-09-04 21:36:30 +0530

    Close #2

---
+++ src/main/java/clinic/controller/LoginFormController.java
@@ -0,0 +1,19 @@
+package clinic.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    public JFXTextField txtName;
+    public JFXPasswordField txtPassword;
+    public JFXButton btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+}
