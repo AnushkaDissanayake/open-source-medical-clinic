94de5c5777a45735467d2129777d3b203489510e
AnushkaDissanayake <anushkamadusanka5@gmail.com>
2022-09-05 17:20:21 +0530

    Close #8

---
+++ src/main/java/clinic/controller/AdminSettingsFormController.java
@@ -0,0 +1,27 @@
+package clinic.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
+public class AdminSettingsFormController {
+    public JFXButton btnHospitalFee;
+    public JFXButton btnAbout;
+    public JFXButton btnPassword;
+    public JFXButton btnManageField;
+    public JFXButton btnDiscount;
+
+    public void btnHospitalFeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDiscountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageFieldOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
+}
